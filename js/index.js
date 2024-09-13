02dd6242da519dad33579d420900e586db02c612
Roland-Hufnagel <roland.hufnagel@gmx.de>
2024-09-13 11:29:55 +0200

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,67 @@
 console.clear();
+const cohort = "Sharks";
+// function declaration:
+function printPersonalData() {
+  const name = "Alex";
+  const age = 25;
+  console.log(name + " is " + age + " years old" + cohort);
+}
+
+// function call
+printPersonalData();
+printPersonalData();
+printPersonalData();
+printPersonalData();
+printPersonalData();
+printPersonalData();
+
+// function declaration with parameters:
+function printPersonalData2(name, age) {
+  console.log(name + " is " + age + " years old");
+}
+
+// function call with arguments:
+printPersonalData2("Rebecca", 20);
+printPersonalData2("Max Müller", 44);
+printPersonalData2("Ibo", 31);
+
+function calculateSum(a, b) {
+  const result = a + b;
+  return result;
+}
+
+//console.log(result);
+const sum1 = calculateSum(5, 6);
+const sum2 = calculateSum(sum1, 100);
+console.log(sum1);
+console.log(sum2);
+
+// function declaration with parameter and return value
+function checkInputString(inputString) {
+  if (inputString.length > 3) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const isValid = checkInputString("Hello");
+console.log(isValid);
+// ...
+console.log("--------------------------");
+greet();
+
+function greet() {
+  console.log("Hello");
+}
+// fat arrow function declaration:
+
+const greetSharks = (name, age) => {
+  console.log("Hello Sharks " + name);
+};
+
+greetSharks("Thilda");
+
+const calc = (a, b) => a + b;
+
+calc(5, 6);
